5cf7610d9c359609043b5df665a11b77ce7072b5
amig03 <32470081+amig03@users.noreply.github.com>
2017-11-08 19:38:24 +0300

    Update tetris_draw.js

--- tetris_draw.js
+++ tetris_draw.js
@@ -1,8 +1,8 @@
-Tetris.prototype.draw = function (options) {
+Tetris.prototype.drawCells = function (options) {
 
 // получение параметров из options
 
-	var canvas_id = options.canvas_id;
+	var canvas_gridID = options.canvas_gridID;
 	var x_cells = options.X_CELLS;
 	var y_cells = options.Y_CELLS;
 	var dx = options.CELL_WIDTH;
@@ -15,18 +15,17 @@ Tetris.prototype.draw = function (options) {
 
 // получение элемента canvas
 
-	var canvas = document.getElementById(canvas_id);
+	var canvas = document.getElementById(canvas_gridID);
 	var ctx = canvas.getContext('2d');
 
-	canvas.setAttribute('width', w);
-	canvas.setAttribute('height', h);
-
-	var scope = this;
 	var i = 0;
 
 // очистка canvas, затем отрисовка линий сетки
 
 	ctx.clearRect(0, 0, w, h);
+
+	// ctx.fillStyle = '#676767';
+	// ctx.fillRect(0,0,w,h);
 
 	for (i = 0; i <= w; i += dx) {
 		ctx.moveTo(i, 0);
@@ -41,8 +40,14 @@ Tetris.prototype.draw = function (options) {
 // задание параметров линий
 
 	ctx.lineWidth = 1;
-	ctx.strokeStyle = '#676767';
+	ctx.strokeStyle = 'white';
 	ctx.stroke();
 
-	return scope;
+	// var canvas_figures = document.getElementById(options.canvas_figuresID);
+	// var ctx_fig = canvas_figures.getContext('2d');
+
+	// ctx_fig.clearRect(0, 0, 400, 800);
+	// ctx_fig.fillStyle = 'green';
+	// ctx_fig.fillRect(10, 10, 10, 10);
+
 }
