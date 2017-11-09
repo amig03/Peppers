c8d85575f72a6ae85a4ddb7dc67702a79ff487e7
amig03 <32470081+amig03@users.noreply.github.com>
2017-11-09 19:34:47 +0300

    Update others.js

--- others.js
+++ others.js
@@ -1,3 +1,24 @@
 Math.randInt = function (max) {
 	return Math.floor(Math.random() * max);
 };
+
+function isOne (num) {
+	return num === 1;
+}
+
+function drawBlock (ctx, x0, y0, x, y) {
+	ctx.strokeStyle = 'white';
+	ctx.fillStyle = 'black';
+	ctx.fillRect(x0, y0, x, y);
+	ctx.strokeRect(x0, y0, x, y);
+}
+
+function addListeners() {
+	document.addEventListener('keydown', onKeyDown);
+	document.addEventListener('keyup', onKeyUp);
+}
+
+function removeListeners() {
+	document.removeEventListener('keydown', onKeyDown);
+	document.removeEventListener('keyup', onKeyUp);
+}
